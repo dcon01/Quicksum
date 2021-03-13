244ceada430d852023a516f9dab9e155ba1508cd
Danny <danny.connolly@my.jcu.edu.au>
2021-03-13 14:52:02 +1100

    first Commit

--- app/src/main/java/au/edu/jcu/sp3406/quicksum/MainActivity.java
+++ app/src/main/java/au/edu/jcu/sp3406/quicksum/MainActivity.java
@@ -3,12 +3,34 @@ package au.edu.jcu.sp3406.quicksum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public int sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void buttonClicked(View view) {
+        Button button = (Button) view;
+        int number = Integer.parseInt(button.getText().toString());
+        sum += number;
+
+        TextView textView = findViewById(R.id.text);
+        String result = "" + sum;
+        textView.setText(result);
+        }
+
+    public void clearButton(View view){
+        TextView textView = findViewById(R.id.text);
+        textView.setText("");
+        sum = 0;
+    }
 }
+
